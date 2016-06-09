597eee63349bfb5c3a0cca3b9c163402423bb7de
Dan <danmedani@gmail.com>
2016-06-09 14:46:11 -0700

    Using new params... he's not bad!

--- tactoe.py
+++ tactoe.py
@@ -20,11 +20,15 @@ class Game:
     
     while True:
       moveRes = self.players[currentPlayer].move(self.board)
+      if moveRes[0] == None:
+        return self.players[currentPlayer].getName()
+      
       print self.players[currentPlayer].getName(), 'moved here:', moveRes[0]
       self.board = moveRes[1]
 
       if self.isWinner():
         print self.players[currentPlayer].getName() + ' has won!!! Good job!'
+        return self.players[currentPlayer].getName()
         break
 
       currentPlayer = 1 - currentPlayer
@@ -83,7 +87,7 @@ class AI:
     return self.cpuName
 
   def move(self, board):
-    highestRanked = -1 * (10 ** 20)
+    highestRanked = -1 * (10 ** 90)
     moveToChoose = None
     
     # Are we forced to win?
@@ -103,8 +107,6 @@ class AI:
       counts = self.getCounts(board, line)
     
       if (counts[0] == 0) and (counts[1] == 3):
-        print 'Cpu is forced. ', line
-          
         for pos in line:
           if board[pos[0]][pos[1]][pos[2]] == None:
 
@@ -113,8 +115,10 @@ class AI:
             return pos, board
 
     # No forced moves... Let's look for a good one
+    moveFound = False
     for move in self.moveList:
       if board[move[0]][move[1]][move[2]] == None:
+        moveFound = True
         tmpBoard = copy.deepcopy(board)
         tmpBoard[move[0]][move[1]][move[2]] = self.cpuName
         # print '     move ', move
@@ -124,8 +128,11 @@ class AI:
           highestRanked = potentialRank
           moveToChoose = move
 
-
-    # print cpuName + ' Moves Here: ', moveToChoose
+    if not moveFound:
+      print 'Cat Game'
+      return None, board
+    # print moveToChoose
+    # print self.cpuName + ' Moves Here: ', moveToChoose
     board[moveToChoose[0]][moveToChoose[1]][moveToChoose[2]] = self.cpuName
     return moveToChoose, board
 
@@ -250,16 +257,12 @@ class AI:
       elif counts[0] * counts[1] > 0:
         rank = rank + 0
       elif counts[0] > 0:
-        rank = rank + cpuParams[counts[0]-1]
+        rank = rank + self.cpuParams[counts[0]-1]
       elif counts[1] > 0:
-        rank = rank - userParams[counts[1]-1]
+        rank = rank - self.userParams[counts[1]-1]
 
     return rank
 
-
-
-cpuParams = [1, 100, 10000, 10 ** 20]
-userParams = [100, 10000, 1000000, 10 ** 10]
 
 lines = []
 # Verts
@@ -351,8 +354,51 @@ lines.append([[3, 2, 0], [2, 2, 1], [1, 2, 2], [0, 2, 3]])
 lines.append([[3, 3, 0], [2, 3, 1], [1, 3, 2], [0, 3, 3]])
 
 boardSize = 4
-p1 = User()
-print p1.getName()
-p2 = AI(boardSize, 'Dave', p1.getName(), cpuParams, userParams, lines)
-game = Game(p1, p2, lines)
-game.play()
+
+bestParams = [56734, 155558, 305346, 446075]
+bestUserParams = [128521, 174904, 225807, 357475]
+
+user = User()
+cpu = AI(boardSize, 'Troll', user.getName(), bestParams, bestUserParams, lines)
+
+# game = Game(cpu, user, lines)
+game = Game(user, cpu, lines)
+print game.play()
+
+# Playing the machines against each other
+# while True:
+#   bar = random.randint(100, 1000000)
+#   champ = AI(boardSize, 'Champ', 'Challenger', bestParams, bestUserParams, lines)
+  
+#   challengerParams = []
+#   lastNum = 1
+#   for i in xrange(4):
+#     newNum = random.randint(lastNum, lastNum + bar)
+#     challengerParams.append(newNum)
+
+#     lastNum = newNum
+
+#   challengerUserParams = []
+#   lastNum = 1
+#   for i in xrange(4):
+#     newNum = random.randint(lastNum, lastNum + bar)
+#     challengerUserParams.append(newNum)
+
+#     lastNum = newNum
+
+#   challenger = AI(boardSize, 'Challenger', 'Champ', challengerParams, challengerUserParams, lines)
+  
+#   print 'The Champ:', bestParams, bestUserParams
+#   print 'The Challenger:', challengerParams, challengerUserParams
+
+#   game = Game(champ, challenger, lines)
+
+#   winner = game.play()
+
+#   print 'Winner: ', winner
+#   if winner == 'Challenger':
+#     print 'New Champ!', winner
+#     bestParams = challengerParams
+#     bestUserParams = challengerUserParams
+
+    
